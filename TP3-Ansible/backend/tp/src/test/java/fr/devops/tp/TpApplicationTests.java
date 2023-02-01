7f005aef5b008aa637a56768fb5bc7ecff0347e4
Francois Cheval <orionpegase7@gmail.com>
2023-02-01 14:02:04 +0100

    add TP3 directory

---
+++ TP3-Ansible/backend/tp/src/test/java/fr/devops/tp/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devops.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
